6a06b36793b3852adb3eecf9bf282e5659a20483
VANSH RAWAT <vansh.rawat2795@gmail.com>
2023-07-25 19:47:09 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,10 +18,11 @@ app.use('/api/bookings', bookingsRoute);
 const path = require("path");
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-    });
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
+
 
 app.listen(port, () => console.log('listening on port', port))
