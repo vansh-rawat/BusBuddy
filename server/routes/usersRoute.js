4ae5575642bd0b041d869b3263a5b8970ac09f1d
VANSH RAWAT <vansh.rawat2795@gmail.com>
2023-07-25 20:06:47 +0530

    Update usersRoute.js

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const User = require('../models/usersModel');
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const authMiddleware = require('../server/middlewares/authMiddleware');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 //register new user
 
